94bec1031c6dcd77566738643fdda06f6d7415a9
pea49596 <peachapong.poolpol@mevis.fraunhofer.de>
2026-02-13 15:03:53 +0000

    Change Path

--- scripts/main_predict_eval.py
+++ scripts/main_predict_eval.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.append(str(PROJECT_ROOT))
+
+
 from mst.utils.ignore_warning import suppress_mst_warnings
 suppress_mst_warnings()
 
